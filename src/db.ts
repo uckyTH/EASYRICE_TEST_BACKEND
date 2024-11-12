6b0a64911f0e90c1ea24d89c5aa0a0f3aed4f070
Domgee <asdfadwf@gmail.com>
2024-11-12 10:57:19 +0700

    update env

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
-
+const uri = process.env.MONGO_URI;
+if (!uri) {
+  console.error("❌ MongoDB URI is not defined in the environment variables.");
+  process.exit(1);
+}
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/riceInspectionDB");
+    const dbName = "riceInspectionDB";
+    await mongoose.connect(uri, { dbName });
+
     console.log("✅ MongoDB connected");
+    await mongoose.connect(uri, { dbName });
   } catch (err) {
     console.error("❌ MongoDB connection error:", err);
     process.exit(1);
